d3c724cb5447ed6a1ba42e731363b85b76882194
spenna <sebapenna19@gmail.com>
2021-07-30 18:14:15 -0300

    Add likes table and endpoint

--- models.py
+++ models.py
@@ -17,8 +17,16 @@ def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
 
+class Like(db.Model):
+    __tablename__ = 'like'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_1 = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    user_2 = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    user_1_liked = db.Column(db.Boolean, nullable=False)
+    user_2_liked = db.Column(db.Boolean, nullable=False)
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
@@ -28,3 +36,5 @@ class User(db.Model):
     gender_interests = db.Column(db.String, nullable=False)
     interests = db.Column(db.String, nullable=True)
     register_date = db.Column(db.String, nullable=True, default=func.now())
+    user_1 = db.relationship('Like',backref='user_1', primaryjoin=id==Like.user_1)
+    user_2 = db.relationship('Like',backref='user_2', primaryjoin=id==Like.user_2)
---
+++ namespaces/likes.py
@@ -0,0 +1,61 @@
+from models import db, Like
+from flask_restx import Namespace, Resource, Model, fields, reqparse
+
+ns = Namespace(
+    name="Likes",
+    path="/meetme/likes",
+    description="Operations related to Meet Me likes",
+)
+
+likes_request_model = Model(
+    "Likes model",
+    {
+        "user_1": fields.String(required=True, description="Id of the user who liked"),
+        "user_2": fields.String(required=True, description="Id of the user who was liked")
+    },
+)
+
+likes_response_model = Model(
+    "Likes model",
+    {
+        "match": fields.Boolean(required=True, description="If a match happened between the users"),
+    },
+)
+
+
+ns.models[likes_request_model.name] = likes_request_model
+ns.models[likes_response_model.name] = likes_response_model
+
+parser = reqparse.RequestParser()
+parser.add_argument('user_1', type=int, location='form')
+parser.add_argument('user_2', type=int, location='form')
+
+
+@ns.route('')
+class Likes(Resource):
+    @ns.expect(likes_request_model)
+    @ns.response(code=200, description='Like registered', model=likes_response_model)
+    def post(self):
+        """
+        Registers user
+        """
+        args = parser.parse_args()
+        args["user_1_liked"] = True
+        args["user_2_liked"] = False
+        id_1 = args["user_1"]
+        id_2 = args["user_2"]
+
+        like = Like.query.filter(Like.user_1 == id_1 and Like.user_2 == id_2).first()
+        if like is None:
+            like = Like.query.filter(Like.user_2 == id_1 and Like.user_1 == id_2).first()
+            if like is None:
+                new_like = Like(args)
+                db.session.add(new_like)
+                db.session.commit()
+                return {"match": False}, 200
+            like.user_2_liked = True
+            db.session.add(like)
+            db.session.commit()
+            return {"match": True}, 200
+
+        return {"match": False}, 200
